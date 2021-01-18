bbcf12ea3d198c8acf7f66acfdfa7c8961270fc3
Nayeong <imtherealk@gmail.com>
2021-01-18 22:25:07 +0900

    test: Add e2e test codes for createAccount

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,25 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { getConnection } from 'typeorm';
+import { UserRole } from 'src/users/entities/user.entity';
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
+const testUser = {
+  email: 'immda@naver.com',
+  password: '12345',
+  role: UserRole.Host,
+};
 
 describe('App (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
+
+  const baseTest = () => request(app.getHttpServer()).post(GRAPHQL_ENDPOINT);
+  const publicTest = (query: string) => baseTest().send({ query });
+  const privateTest = (query: string, token: string = jwtToken) =>
+    baseTest().set('x-jwt', token).send({ query });
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
@@ -13,6 +29,11 @@ describe('App (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
   });
 
   describe('Podcasts Resolver', () => {
@@ -29,7 +50,63 @@ describe('App (e2e)', () => {
   describe('Users Resolver', () => {
     it.todo('me');
     it.todo('seeProfile');
-    it.todo('createAccount');
+    describe('createAccount', () => {
+      it('should create account', () => {
+        return publicTest(`
+          mutation {
+            createAccount(input:{
+              email: "${testUser.email}",
+              password: "${testUser.password}",
+              role: ${testUser.role}
+            }
+            ){
+              ok
+              error
+            }
+          }
+        `)
+          .expect(200)
+          .expect(res => {
+            const {
+              body: {
+                data: { createAccount },
+              },
+            } = res;
+            expect(createAccount).toEqual({
+              ok: true,
+              error: null,
+            });
+          });
+      });
+
+      it('should fail if account already exists', () => {
+        return publicTest(`
+          mutation {
+            createAccount(input:{
+              email: "${testUser.email}",
+              password: "${testUser.password}",
+              role: ${testUser.role}
+            }
+            ){
+              ok
+              error
+            }
+          }
+        `)
+          .expect(200)
+          .expect(res => {
+            const {
+              body: {
+                data: { createAccount },
+              },
+            } = res;
+            expect(createAccount).toEqual({
+              ok: false,
+              error: 'There is a user with that email already',
+            });
+          });
+      });
+    });
     it.todo('login');
     it.todo('editProfile');
   });
